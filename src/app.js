838474eeede5bbce4a7d8945c6ab3351f78314ea
LiorBar230 <Liorbar230@gmail.com>
2022-11-13 21:41:07 -0500

    trying to connect js file

--- src/app.js
+++ src/app.js
@@ -34,5 +34,3 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showSearchCityTemp);
 }
-
-alert("hello world");
