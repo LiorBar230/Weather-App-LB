c6fc7cde5c8c080505278972e3a991a6de429096
LiorBar230 <Liorbar230@gmail.com>
2022-11-13 21:42:33 -0500

    -

--- src/app.js
+++ src/app.js
@@ -34,3 +34,5 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showSearchCityTemp);
 }
+
+alert("hello");
