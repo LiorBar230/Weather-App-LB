f0bfd3a39d74d5a71eb1cf93233c6a2cd2e18a29
LiorBar230 <Liorbar230@gmail.com>
2022-11-20 13:23:44 -0500

    correcting the humidity element

--- src/app.js
+++ src/app.js
@@ -27,8 +27,8 @@ citySearchEngine.addEventListener("submit", showSearchCityName);
 function showSearchCityTemp(response) {
   let currentTempDisplay = document.querySelector("strong#temperature");
   let temperature = Math.round(response.data.main.temp);
-  let currentHumidtyDisplay = document.querySelector("span#currentHumidity");
-  let humidty = Math.round(response.data.main.humidty);
+  let currentHumidityDisplay = document.querySelector("span#currentHumidity");
+  let humidity = Math.round(response.data.main.humidity);
   let currentWindDisplay = document.querySelector("span#currentWind");
   let wind = Math.round(response.data.wind.speed);
   let tempMinDisplay = document.querySelector("span#currentLowTemp");
@@ -36,10 +36,10 @@ function showSearchCityTemp(response) {
   let tempHighDisplay = document.querySelector("span#currentHighTemp");
   let tempHigh = Math.round(response.data.main.temp_max);
   currentTempDisplay.innerHTML = `${temperature}`;
-  currentHumidtyDisplay.innerHTML = `Humidty: ${humidty}%`;
+  currentHumidityDisplay.innerHTML = `Humidty: ${humidity}%`;
   currentWindDisplay.innerHTML = `Wind: ${wind} Km/h`;
   tempMinDisplay.innerHTML = `${minTemp}°`;
-  tempHighDisplay.innerHTML = `${tempHigh}°`;
+  tempHighDisplay.innerHTML = `${tempHigh}°/`;
 }
 function searchCity(city) {
   let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
